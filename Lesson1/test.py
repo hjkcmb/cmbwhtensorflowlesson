9dbc7b8eb22e87e0b2b3b90f305e3aab383f200d
cmbwh <hjk1130@gmail.com>
2018-02-24 17:20:10 +0800

    test2

--- Lesson1/test.py
+++ Lesson1/test.py
@@ -4,5 +4,6 @@ x=tf.constant([[1,2]])
 y=tf.constant([[3],[4]])
 s=tf.matmul(x,y)
 
-with tf.Session() as sess:
-    print(sess.run(s))
+sess=tf.Session()
+print(x)
+print(sess.run(s))
--- Lesson2/example1.py
+++ Lesson2/example1.py
@@ -6,13 +6,9 @@ import matplotlib.pyplot as plt
 x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data*0.1 + 0.3
 
-# plot data
-#plt.scatter(x_data, y_data)
-#plt.show()
 ### create tensorflow structure start ###
 Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 biases = tf.Variable(tf.zeros([1]))
-
 y = Weights*x_data + biases
 
 loss = tf.reduce_mean(tf.square(y-y_data))
